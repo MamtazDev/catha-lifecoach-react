827d0153c660786e37cf91e9f2903be1757bc2ce
nipaaa <akternipa941@gmail.com>
2023-08-21 15:51:15 +0600

    data protect add

---
+++ src/Pages/ProtectData.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ProtectData = () => {
+  return (
+    <section className="imprint">
+      <div className="darkBoxImprint"></div>
+      <div className="container">
+        <div style={{ height: "500px", margin: "auto" }}></div>
+      </div>
+    </section>
+  );
+};
+
+export default ProtectData;
