6e9d63de0d569f93e1b7d3face084781fe42fd6e
nipaaa <akternipa941@gmail.com>
2023-08-22 12:36:22 +0600

    add link

--- src/Components/Coaching/CoachingFooter.jsx
+++ src/Components/Coaching/CoachingFooter.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import before from "../../assets/before.png";
 import after from "../../assets/after.png";
+import { Link } from "react-router-dom";
 
 const CoachingFooter = () => {
   return (
@@ -17,14 +18,19 @@ const CoachingFooter = () => {
           </p>
 
           <div className="next_btn d-flex justify-content-between align-items-center">
-            <button>
-              <img className="img-fluid me-3" src={before} alt="Left Arrow" />
-              Zurück
-            </button>
-            <button className="after_btn">
-              zum Coaching
-              <img className="img-fluid ms-3" src={after} alt="Right Arrow" />
-            </button>
+            <Link to="/about">
+              {" "}
+              <button>
+                <img className="img-fluid me-3" src={before} alt="Left Arrow" />
+                Zurück
+              </button>
+            </Link>
+            <Link to="/contact">
+              <button className="after_btn">
+                zum KOntakt
+                <img className="img-fluid ms-3" src={after} alt="Right Arrow" />
+              </button>
+            </Link>
           </div>
         </div>
       </div>
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import contactbanner from "../assets/contact-banner.png";
 import before from "../assets/before.png";
 import after from "../assets/after.png";
+import { Link } from "react-router-dom";
 
 const Contact = () => {
   return (
@@ -71,14 +72,20 @@ const Contact = () => {
             style={{ maxWidth: "760px", margin: "auto" }}
             className="next_btn d-flex justify-content-between align-items-center"
           >
-            <button>
-              <img className="img-fluid me-3" src={before} alt="Left Arrow" />
-              Zurück
-            </button>
-            <button className="after_btn">
-              zum Coaching
-              <img className="img-fluid ms-3" src={after} alt="Right Arrow" />
-            </button>
+            <Link to="/coaching">
+              {" "}
+              <button>
+                <img className="img-fluid me-3" src={before} alt="Left Arrow" />
+                Zurück
+              </button>
+            </Link>
+            <Link to="/imprint">
+              {" "}
+              <button className="after_btn">
+                zum Impressum
+                <img className="img-fluid ms-3" src={after} alt="Right Arrow" />
+              </button>
+            </Link>
           </div>
         </div>
       </div>
